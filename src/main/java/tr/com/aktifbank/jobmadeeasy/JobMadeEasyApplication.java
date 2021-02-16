b170384739d9215beecdc207e51d1c174456f552
Aytac Genc <aytacgenc1@gmail.com>
2021-02-16 23:47:19 +0300

    initial commit

---
+++ src/main/java/tr/com/aktifbank/jobmadeeasy/JobMadeEasyApplication.java
@@ -0,0 +1,13 @@
+package tr.com.aktifbank.jobmadeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMadeEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMadeEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/com/aktifbank/jobmadeeasy/JobMadeEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.aktifbank.jobmadeeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMadeEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
