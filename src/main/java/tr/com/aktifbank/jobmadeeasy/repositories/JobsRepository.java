db5eb4844a0ec9911aaf5f035e471d45f32f29bb
Aytac Genc <aytacgenc1@gmail.com>
2021-03-03 02:29:55 +0300

    removed repo file for future impl

--- src/main/java/tr/com/aktifbank/jobmadeeasy/repositories/JobsRepository.java
+++
@@ -1,7 +0,0 @@
-package tr.com.aktifbank.jobmadeeasy.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import tr.com.aktifbank.jobmadeeasy.model.Jobs;
-
-public interface JobsRepository extends CrudRepository<Integer, Jobs> {
-}
